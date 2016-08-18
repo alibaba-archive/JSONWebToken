b217edbc84a1ea3e3f4b5b1bf4512f3142af8737
Zhu Shengqi <shengqi@teambition.com>
2016-08-18 16:08:39 +0800

    Initial Commit

---
+++ JSONWebToken/JSONWebToken.h
@@ -0,0 +1,19 @@
+//
+//  JSONWebToken.h
+//  JSONWebToken
+//
+//  Created by Zhu Shengqi on 8/18/16.
+//  Copyright © 2016 dia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONWebToken.
+FOUNDATION_EXPORT double JSONWebTokenVersionNumber;
+
+//! Project version string for JSONWebToken.
+FOUNDATION_EXPORT const unsigned char JSONWebTokenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONWebToken/PublicHeader.h>
+
+
