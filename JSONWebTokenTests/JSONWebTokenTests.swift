a896725b9007ec7d3b623bcaaed29f05a9c3bd98
wzxjiang <wzxjiang@foxmail.com>
2018-04-09 14:27:33 +0800

    add unit test

--- JSONWebTokenTests/JSONWebTokenTests.swift
+++ JSONWebTokenTests/JSONWebTokenTests.swift
@@ -13,24 +13,55 @@ class JSONWebTokenTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testEncode() {
+        let input1 = "test"
+        
+        guard let data1 = input1.data(using: .utf8) else {
+            assertionFailure()
+            return
         }
+        
+        let output1 = base64_encode(data1)
+        
+        XCTAssert(output1 == "dGVzdA")
+        
+        //
+        
+        let input2 = "helloworld"
+        
+        guard let data2 = input2.data(using: .utf8) else {
+            assertionFailure()
+            return
+        }
+        
+        let output2 = base64_encode(data2)
+        
+        XCTAssert(output2 == "aGVsbG93b3JsZA")
     }
     
+    func testDecode() {
+        let input1 = "dGVzdA=="
+        
+        guard let data1 = base64_decode(input1),
+            let output1 = String(data: data1, encoding: .utf8) else {
+            assertionFailure()
+            return
+        }
+        
+        XCTAssert(output1 == "test")
+        
+        //
+        
+        let input2 = "aGVsbG93b3JsZA"
+        
+        guard let data2 = base64_decode(input2),
+            let output2 = String(data: data2, encoding: .utf8) else {
+                assertionFailure()
+                return
+        }
+        
+        XCTAssert(output2 == "helloworld")
+    }
 }
